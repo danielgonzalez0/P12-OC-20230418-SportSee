0621b725e80a742813a96251ccfccb7ca63b6253
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-04-20 20:52:18 +0200

    update user performance data model

--- src/scripts/2_models/mockedData/UserPerformanceDataMocked.js
+++ src/scripts/2_models/mockedData/UserPerformanceDataMocked.js
@@ -1,5 +1,26 @@
 import { PerformanceData } from '../PerformanceData';
 
+/**
+ * Map the kind value from english to french using a mapping table
+ * @param {string} kind
+ * @returns {string} kind translate in french
+ */
+export const kindMappingTable = (kind) => {
+  const mapping = [
+    { en: 'cardio', fr: 'Cardio' },
+    { en: 'energy', fr: 'Energie' },
+    { en: 'endurance', fr: 'Endurance' },
+    { en: 'strength', fr: 'Force' },
+    { en: 'speed', fr: 'Vitesse' },
+    { en: 'intensity', fr: 'Intensité' },
+  ];
+  return mapping.map((mappedKind) => {
+    if (kind === mappedKind.en) {
+      return mappedKind.fr;
+    }
+  });
+};
+
 export class UserPerformanceDataMocked {
   constructor(user) {
     this._id = user[0].userId;
@@ -20,17 +41,17 @@ export class UserPerformanceDataMocked {
       const data = new PerformanceData(element);
       switch (data.kind) {
         case 1:
-          return { value: data.value, kind: this._kind[1] };
+          return { value: data.value, kind: kindMappingTable(this._kind[1]) };
         case 2:
-          return { value: data.value, kind: this._kind[2] };
+          return { value: data.value, kind: kindMappingTable(this._kind[2]) };
         case 3:
-          return { value: data.value, kind: this._kind[3] };
+          return { value: data.value, kind: kindMappingTable(this._kind[3]) };
         case 4:
-          return { value: data.value, kind: this._kind[4] };
+          return { value: data.value, kind: kindMappingTable(this._kind[4]) };
         case 5:
-          return { value: data.value, kind: this._kind[5] };
+          return { value: data.value, kind: kindMappingTable(this._kind[5]) };
         case 6:
-          return { value: data.value, kind: this._kind[6] };
+          return { value: data.value, kind: kindMappingTable(this._kind[6]) };
         default:
           return { data };
       }
